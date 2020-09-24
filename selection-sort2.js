7c112724a53b07f5845388f0d6d6fcaf07910494
JPCarrijo <carrijojp@gmail.com>
2020-09-24 23:34:38 +0000

    (24/09) Selection Sort com função de comparação

--- selection-sort2.js
+++ selection-sort2.js
@@ -1,4 +1,13 @@
-function selectionSort(vetor) {
+/*
+    Função de comparação fnComp
+    - Recebe dois valores para comparação
+    - Retorno:
+        - true se o PRIMEIRO valor for MAIOR que segundo
+        - false caso contrário
+
+*/
+
+function selectionSort(vetor, fnComp) {
     let passadas = 0, comparacoes = 0, totalTrocas = 0
 
     // Função que encontra o menor número em um segmento de vetor (subvetor)
@@ -6,7 +15,8 @@ function selectionSort(vetor) {
     function encontrarMenor(vetor, inicio) {
         let posMenor = inicio
         for (let i = inicio + 1; i < vetor.length; i++) {
-            if (vetor[i] < vetor[posMenor]) posMenor = i
+            //if(vetor[i] < vetor[posMenor]) posMenor = i
+            if (!fnComp(vetor[i], vetor[posMenor])) posMenor = i
             comparacoes++
         } return posMenor
     }
@@ -21,12 +31,13 @@ function selectionSort(vetor) {
         // Caso o valor encontrado seja menor que o valor atual,
         // procede-se à troca
         comparacoes++
-        if (vetor[posMenor] < vetor[i]) {
+        //if (vetor[posMenor] < vetor[i]) {
+        if (fnComp(vetor[i], vetor[posMenor])) { // Ordem dos argumentos invertida
             // Permuta de valores usando desestruturação
             [vetor[posMenor], vetor[i]] = [vetor[i], vetor[posMenor]]
             totalTrocas++
         }
-    } console.log({passadas, comparacoes, totalTrocas})
+    } console.log({ passadas, comparacoes, totalTrocas })
 }
 
 const nums = [56, 78, 44, 23, 99, 14, 60, 37, 6, 82, 31, 65]
@@ -37,15 +48,15 @@ console.time('Nums')
 selectionSort(nums)
 console.timeEnd('Nums')
 */
-const nomes = require('./dados/100-mil-nomes')
 
-console.time('Teste nomes')
-selectionSort(nomes)
-console.timeEnd('Teste nomes')
+const candidatos = require('./dados/candidatos-2018')
+
+console.time('Teste candidatos')
+selectionSort(candidatos, (x, y) => x.NM_CANDIDATO > y.NM_CANDIDATO)
+console.timeEnd('Teste candidatos')
 // Medindo a memória utilizada pelo progrma
 let memoria = process.memoryUsage().heapUsed / 1024 / 1024
-console.log(nomes)
+console.log(candidatos)
 console.log('Memória utilizada (MB):', memoria)
 
 //console.log(nums)
-
